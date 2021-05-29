83b359f58645df932bba84a5ff692411837a33b4
Jake Scott <bemybanker@gmail.com>
2021-05-29 09:43:51 -0500

    commit before mdx transformation

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,16 @@
 import React from "react"
 import Layout from "../components/layout/layout"
-
+import ProjectCard from "../components/projects/projectcard"
 
 
 const IndexPage = () => {
   return (
     <Layout>
       <h1>Welcome to my site</h1>
+      <h2>Latest Projects</h2>
       <h3>Here you can explore all of my projects and interests i've been working on</h3>
+      <ProjectCard/>
+      <h2>Here are some of my recent blog posts</h2>
     </Layout>
   )
 }
