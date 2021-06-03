811942c3f83769f9c72d656299bed77336297790
Jake Scott <bemybanker@gmail.com>
2021-06-03 16:01:13 -0500

    Added mobile menu, working on home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,30 @@
 import React from "react"
+import { Link } from "gatsby"
 import Layout from "../components/layout/layout"
 import ProjectCard from "../components/projects/projectcard"
-
 
 const IndexPage = () => {
   return (
     <Layout>
       <h1>Welcome to my site</h1>
-      <h2>Latest Projects</h2>
-      <h3>Here you can explore all of my projects and interests i've been working on</h3>
-      <ProjectCard/>
+      <h2>
+        Hi, my name is Jake Scott; engineer by design, full time student at
+        NSCC in Mechanical Engineering, and all-around general handyman.
+      </h2>
+      <h2>
+        My latest projects can be found on my <Link to="/blog">blog</Link> and
+        my services can be found <Link to="/services">here</Link>
+      </h2>
+      <h3>
+        I have an array of different specialities within my skillset which
+        include but not limited to, <Link to="/welding">welding</Link>, {" "}
+        <Link to="/electrical">electrical</Link>,{" "}
+        <Link to="plumbing">plumbing</Link>, and {" "}
+        <Link to="/auto-repair">automotive repair and diagnosis</Link>. All of
+        which can be found in more detail <Link to="/services">here</Link>
+        
+      </h3>
+      <ProjectCard />
       <h2>Here are some of my recent blog posts</h2>
     </Layout>
   )
